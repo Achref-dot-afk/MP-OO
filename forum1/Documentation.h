b8db6dec1f417ec12d75f12bbd8986b7eff9fd2c
unknown <ayoub.benemnaelaguerbi@etudiant-enit.utm.tn>
2023-04-29 18:31:18 +0200

    require some changes

---
+++ forum1/Documentation.h
@@ -0,0 +1,18 @@
+#ifndef DOCUMENTATION_H_INCLUDED
+#define DOCUMENTATION_H_INCLUDED
+#include"string.h"
+#include "site.h"
+#include"vector"
+class Documentation:public Site
+{
+    //on va ajouter la classe rubrique ici
+    Site SD;
+public:
+    Documentation (int id_d);
+
+};
+
+
+#endif // DOCUMENTATION_H_INCLUDED
+#include <iostream>
+
